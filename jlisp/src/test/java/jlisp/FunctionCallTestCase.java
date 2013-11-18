dfbb823edd988713d67c47cda65f693be791aeaa
bonapeti <bonapeti@gmail.com>
2013-11-18 12:45:08 +0100

    - remove import

--- jlisp/src/test/java/jlisp/FunctionCallTestCase.java
+++ jlisp/src/test/java/jlisp/FunctionCallTestCase.java
@@ -3,12 +3,8 @@ package jlisp;
 
 import static org.junit.Assert.assertSame;
 import static org.mockito.Matchers.eq;
-import static org.mockito.Matchers.isA;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-
-import java.util.Collection;
-import java.util.LinkedList;
 
 import org.junit.Test;
 
