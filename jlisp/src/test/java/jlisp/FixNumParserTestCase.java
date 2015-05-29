a91d7e5e919d1e4c4737598d3a2f7db518d9f0ab
bonapet <bonapet@gmail.com>
2015-05-29 14:02:22 +0200

    - added missing unit test files

---
+++ jlisp/src/test/java/jlisp/FixNumParserTestCase.java
@@ -0,0 +1,30 @@
+package jlisp;
+
+import static org.junit.Assert.*;
+
+import java.util.Stack;
+
+import org.junit.Test;
+
+public class FixNumParserTestCase {
+
+	@Test
+    public void parse1() {
+        Stack<LispObject> stack = new Stack<LispObject>();
+        FixnumParser parser = new FixnumParser();
+        CharIterator chars = new CharIterator("1");
+        parser.parse(chars, stack);
+        assertEquals(new Fixnum(1),stack.pop());
+        assertEquals(1, chars.getCurrentPosition());
+    }
+	
+	@Test
+    public void parseMinus1() {
+        Stack<LispObject> stack = new Stack<LispObject>();
+        FixnumParser parser = new FixnumParser();
+        CharIterator chars = new CharIterator("-1");
+        parser.parse(chars, stack);
+        assertEquals(new Fixnum(-1),stack.pop());
+        assertEquals(2, chars.getCurrentPosition());
+    }
+}
