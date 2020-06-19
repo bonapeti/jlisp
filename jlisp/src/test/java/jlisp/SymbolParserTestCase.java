dc602752cfd2c427cad439d5cbdc59b42df19283
Peter Bona <bonapeti@gmail.com>
2020-06-19 03:12:48 +0200

    - Refactoring SymbolParserTestCase

--- jlisp/src/test/java/jlisp/SymbolParserTestCase.java
+++ jlisp/src/test/java/jlisp/SymbolParserTestCase.java
@@ -8,97 +8,57 @@ import org.junit.jupiter.api.Test;
 
 public class SymbolParserTestCase {
 
-    @Test
-    public void to_string() {
-        SymbolParser symbolParser = new SymbolParser();
-        assertEquals("symbol",symbolParser.toString());
-    }
-    
+    private static final String EXPECTED_SYMBOL_BUT_FOUND_END_OF_FILE = "Expected SYMBOL but found end of file.";
+    private final Stack<LispObject> stack = new Stack<>();
+    private final SymbolParser parser = new SymbolParser();
+
     @Test
     public void parseEmpty() {
-        Stack<LispObject> stack = new Stack<>();
-        SymbolParser symbolParser = new SymbolParser();
         LispCode chars = new LispCode("");
         try {
-            symbolParser.parse(chars, stack);
-            fail("Should have failed with 'Expected SYMBOL but found end of file.'");
+            parser.parse(chars, stack);
+            fail("Should have failed with '" + EXPECTED_SYMBOL_BUT_FOUND_END_OF_FILE + "'");
         } catch (ParseException pe) {
-            assertEquals("Expected SYMBOL but found end of file.", pe.getMessage());
-            assertEquals(0, chars.getCurrentPosition());
+            assertEquals(EXPECTED_SYMBOL_BUT_FOUND_END_OF_FILE, pe.getMessage());
+            assertTrue(chars.isAtBeginning());
         }
     }
 
     
     @Test
     public void parseArg() {
-        Stack<LispObject> stack = new Stack<>();
-        SymbolParser symbolParser = new SymbolParser();
-        LispCode chars = new LispCode("arg ");
-        symbolParser.parse(chars, stack);
+
+        LispCode chars = new LispCode("arg");
+        parser.parse(chars, stack);
         assertEquals(new Symbol("arg"),stack.pop());
-        assertEquals(3, chars.getCurrentPosition());
-    }
-    
-    @Test
-    public void parseArg1() {
-        Stack<LispObject> stack = new Stack<>();
-        SymbolParser symbolParser = new SymbolParser();
-        symbolParser.parse(new LispCode("arg1"), stack);
-        assertEquals(new Symbol("arg1"),stack.pop());
-    }
-    
-    @Test
-    public void parseHelloHyphenWorld() {
-        Stack<LispObject> stack = new Stack<>();
-        SymbolParser symbolParser = new SymbolParser();
-        symbolParser.parse(new LispCode("hello-world"), stack);
-        assertEquals(new Symbol("hello-world"),stack.pop());
+        assertFalse(chars.hasMore());
     }
 
-    
-    
     @Test
     public void space() {
-        SymbolParser parser = new SymbolParser();
-        assertThrows(ParseException.class, () -> parser.parse(new LispCode(" "), new Stack<LispObject>()));
+        assertThrows(ParseException.class, () -> parser.parse(new LispCode(" "), stack));
     }
     
     @Test
     public void spaceA() {
-        SymbolParser parser = new SymbolParser();
-        assertThrows(ParseException.class, () -> parser.parse(new LispCode(" a"), new Stack<LispObject>()));
+        assertThrows(ParseException.class, () -> parser.parse(new LispCode(" a"), stack));
     }
     
     @Test
     public void quoted() {
-        SymbolParser parser = new SymbolParser();
-        assertThrows(ParseException.class, () -> parser.parse(new LispCode("'a"), new Stack<LispObject>()));
+        assertThrows(ParseException.class, () -> parser.parse(new LispCode("'a"), stack));
     }
-    
-    @Test
-    public void a() {
-        SymbolParser parser = new SymbolParser();
-        Stack<LispObject> stack = new Stack<>();
-        parser.parse(new LispCode("a"), stack);
-        assertFalse(stack.isEmpty());
-        assertEquals(new Symbol("a"), stack.pop());
-    }
-    
+
     @Test
     public void aSpace() {
-        SymbolParser parser = new SymbolParser();
-        Stack<LispObject> stack = new Stack<>();
-        parser.parse(new LispCode("a "), stack);
-        assertFalse(stack.isEmpty());
+
+        LispCode chars = new LispCode("a ");
+        parser.parse(chars, stack);
+
         assertEquals(new Symbol("a"), stack.pop());
+        assertTrue(stack.isEmpty());
+
+        assertTrue(chars.hasMore());
     }
-    
-    @Test
-    public void letStar() {
-        SymbolParser parser = new SymbolParser();
-        Stack<LispObject> stack = new Stack<>();
-        parser.parse(new LispCode("let* "), stack);
-        assertFalse(stack.isEmpty());
-        assertEquals(new Symbol("let*"), stack.pop());
-    }
+
 }
